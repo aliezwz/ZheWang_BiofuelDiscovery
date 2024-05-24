031e48e4f57aa3e15a9180e2e3b532e13b103889
aliezwz <zhe61507@gmail.com>
2024-05-24 09:57:43 +0100

    add files

---
+++ MolGans.py
@@ -0,0 +1,83 @@
+
+
+import deepchem
+deepchem.__version__
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from collections import OrderedDict
+
+import deepchem as dc
+import deepchem.models
+from deepchem.models import BasicMolGANModel as MolGAN
+from deepchem.models.optimizers import ExponentialDecay
+import tensorflow as tf
+from tensorflow import one_hot
+from rdkit import Chem
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import Draw
+
+from deepchem.feat.molecule_featurizers.molgan_featurizer import GraphMatrix
+
+# Download from MolNet
+# Try tox21 or LIPO dataset
+tasks, datasets, transformers = dc.molnet.load_tox21()
+df = pd.read_csv("Smiles list.csv")
+
+num_atoms = 12
+print(df)
+
+# data = df[['smiles']].sample(4000, random_state=42)
+data = df
+
+# create featurizer
+feat = dc.feat.MolGanFeaturizer(max_atom_count=num_atoms, atom_labels=[0, 5, 6, 7, 8, 9, 11, 12, 13, 14]) #15, 16, 17, 19, 20, 24, 29, 35, 53, 80])
+
+smiles = data['SMILES'].values
+
+filtered_smiles = [x for x in smiles if Chem.MolFromSmiles(x).GetNumAtoms() < num_atoms]
+
+# featurize molecules
+features = feat.featurize(filtered_smiles)
+
+indices = [ i for i, data in enumerate(features) if type(data) is GraphMatrix ]
+print(indices)
+features = [features[i] for i in indices]
+
+# create model
+gan = MolGAN(learning_rate=ExponentialDecay(0.001, 0.9, 5000), vertices=num_atoms)
+dataset = dc.data.NumpyDataset([x.adjacency_matrix for x in features],[x.node_features for x in features])
+
+def iterbatches(epochs):
+    for i in range(epochs):
+        for batch in dataset.iterbatches(batch_size=gan.batch_size, pad_batches=True):
+            adjacency_tensor = one_hot(batch[0], gan.edges)
+            node_tensor = one_hot(batch[1], gan.nodes)
+            yield {gan.data_inputs[0]: adjacency_tensor, gan.data_inputs[1]:node_tensor}
+
+gan.fit_gan(iterbatches(25), generator_steps=0.2, checkpoint_interval=5000)
+generated_data = gan.predict_gan_generator(1000)
+
+nmols = feat.defeaturize(generated_data)
+print("{} molecules generated".format(len(nmols)))
+
+nmols = list(filter(lambda x: x is not None, nmols))
+
+# currently training is unstable so 0 is a common outcome
+print ("{} valid molecules".format(len(nmols)))
+
+nmols_smiles = [Chem.MolToSmiles(m) for m in nmols]
+nmols_smiles_unique = list(OrderedDict.fromkeys(nmols_smiles))
+nmols_viz = [Chem.MolFromSmiles(x) for x in nmols_smiles_unique]
+print ("{} unique valid molecules".format(len(nmols_viz)))
+
+# Generate image
+img = Draw.MolsToGridImage(nmols_viz[0:100], molsPerRow=5, subImgSize=(250, 250), maxMols=100, legends=None, returnPNG=False)
+
+# Display image using matplotlib
+plt.figure(figsize=(12, 12))
+plt.imshow(img)
+plt.axis('off')
+plt.show()
